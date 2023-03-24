8da271c1b774283203db47c323c636b83fafe7df
xp <2608415766@qq.com>
2023-03-24 09:23:28 +0800

    second commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("我要被提交到Git仓库");
+        System.out.println("我是第二版");
     }
 }
