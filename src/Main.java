cd9452c94e57e1c6258270a22b8f6c9f65ff609e
xp <2608415766@qq.com>
2023-03-24 09:31:18 +0800

    first commit hot-fix

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("我要被提交到Git仓库");
         System.out.println("我是第二版");
         System.out.println("我是第三版");
+        System.out.println("我是hot-fix分支");
     }
 }
